3d8d53330dcf6022ede615f4ae5f8ece890081b0
Diaz,Ray <Ray.Diaz@Cerner.com>
2014-10-24 17:38:05 -0500

    Deploying to sonatype

---
+++ project/addServer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import sys
+import os
+import os.path
+
+if os.environ["TRAVIS_SECURE_ENV_VARS"] == "false":
+  print "no secure env vars available, skipping deployment"
+  sys.exit()
+
+homedir = os.path.expanduser("~")
+
+host = os.environ["HOST"]
+user = os.environ["SONATYPE_USERNAME"]
+password = os.environ["SONATYPE_PASSWORD"]
+
+f = open(homedir + '/.sbt/credentials', 'w')
+f.write("host="+host+"\nuser="+user+"\npass="+password+"\n")
+f.close()
